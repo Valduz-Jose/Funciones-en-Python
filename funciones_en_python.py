92015ffe6c2dc2bed91bb987089b6b944f6c9944
Jose Valduz <jose.valduz@unet.edu.ve>
2026-03-17 11:12:43 -0400

    Agregar archivo inicial con funciones de saludo en Python

---
+++ funciones_en_python.py
@@ -0,0 +1,13 @@
+# Son bloques de codigo para realizar una tarea en particular, se pueden reutilizar y organizar el codigo de manera mas eficiente
+
+# Ventajas de las funciones: Modularidad Reutilizacion de codigo, Mantenimiento, Parametrizacion, Colaboracion, Legibilidad
+
+print("Funciones en Python")
+
+def saludar_mi_funcion():#Firma del metodo
+  # Cuerpo de la funcion
+  print("Hola, esta es mi funcion")
+
+# Programa principal
+saludar_mi_funcion()
+  
